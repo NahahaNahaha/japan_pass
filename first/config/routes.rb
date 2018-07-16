032632e47b2b16704941aafa44390edc1a0939c2
EC2 Default User <ec2-user@ip-172-31-16-27.us-east-2.compute.internal>
2018-07-16 02:48:40 +0000

    new map test

--- first/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
-  root "home#index" 
-  
-  get"/attack", to: "home#attack" 
-  get"/defense", to: "home#defense"
-
-end
